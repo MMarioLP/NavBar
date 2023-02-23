0b70a75a95877cae3b1dc72f8e376e92aa0e42d8
MMarioLP <am4063228@gmail.com>
2023-02-23 10:11:28 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './component/nav.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+    
+  );
+}
+
+export default App;
